d3f39aef5410d0f9faf6464a992f3ee2acd21173
aspekwow <aspekwowo@gmail.com>
2024-03-04 08:08:03 -0600

    solucion de errores

--- pages/_document.js
+++ pages/_document.js
@@ -4,46 +4,46 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
-        <Head>
-
-          <link
-            href="/ico/favicon.ico"
-            rel="shortcut icon"
-            type="image/x-icon"
-          />
-          <link
-            href="/fonts/ProximaNova-Regular.woff2"
-            as="font"
-            type="font/woff2"
-            rel="preload"
-            crossOrigin=""
-          />
-          <link
-            href="/fonts/ProximaNova-Semibold.woff2"
-            as="font"
-            type="font/woff2"
-            rel="preload"
-            crossOrigin=""
-          />
-          <link
-            href="/fonts/ProximaNova-Bold.woff2"
-            as="font"
-            type="font/woff2"
-            rel="preload"
-            crossOrigin=""
-          />
-        </Head>
-        <meta
-          name="description"
-          content="PCStore.mx Tienda líder en cómputo, accesorios, hardware, tecnología y más. Compra protegida, envíos asegurados y pagos seguros con los mejores precios, productos y marcas."
-        />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+			<Html>
+				<Head>
+					<link
+						href='/ico/favicon.ico'
+						rel='shortcut icon'
+						type='image/x-icon'
+					/>
+					<link
+						href='/fonts/ProximaNova-Regular.woff2'
+						as='font'
+						type='font/woff2'
+						rel='preload'
+						crossOrigin=''
+					/>
+					<link
+						href='/fonts/ProximaNova-Semibold.woff2'
+						as='font'
+						type='font/woff2'
+						rel='preload'
+						crossOrigin=''
+					/>
+					<link
+						href='/fonts/ProximaNova-Bold.woff2'
+						as='font'
+						type='font/woff2'
+						rel='preload'
+						crossOrigin=''
+					/>
+					<meta name='viewport' content='width=device-width' />
+					<meta
+						name='description'
+						content='PCStore.mx Tienda líder en cómputo, accesorios, hardware, tecnología y más. Compra protegida, envíos asegurados y pagos seguros con los mejores precios, productos y marcas.'
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
   }
 }
 
