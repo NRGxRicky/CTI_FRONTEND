06fc75ec779f84308b2c3384d3baec3843b85cff
aspekwow <aspekwowo@gmail.com>
2024-05-11 17:35:09 -0600

    solucion de errores

--- pages/[productId].js
+++ pages/[productId].js
@@ -90,9 +90,6 @@ const ProductItem = ({ item }) => {
 						/>
 					</div>
 				)}
-				{getComputedStyle(document.documentElement).getPropertyValue('--sar')}
-				{getComputedStyle(document.documentElement).getPropertyValue('--sat')}
-				{getComputedStyle(document.documentElement).getPropertyValue('--sab')}
 				{getComputedStyle(document.documentElement).getPropertyValue('--sal')}
 				<DetailProduct
 					item={item}
