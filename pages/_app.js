00d60b010d8ce66059ce67d792eb580dd8b62cc4
aspekwow <aspekwowo@gmail.com>
2024-05-11 16:45:09 -0600

    solucion de errores

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }) {
 			<Head>
 				<meta
 					name='viewport'
-					content='width=device-width, maximum-scale=1, minimum-scale=1, initial-scale=1, height=device-height, user-scalable=no, shrink-to-fit=no viewport-fit=cover'
+					content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover'
 				/>
 			</Head>
 			<AuthProvider>
