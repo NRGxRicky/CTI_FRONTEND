7e8c81cf001879444c17445c7dafa1c3279498c5
aspekwow <aspekwowo@gmail.com>
2024-12-03 13:21:44 -0600

    componente register step 1

--- pages/profile/verify-email/resend-email/index.js
+++ pages/profile/verify-email/resend-email/index.js
@@ -98,7 +98,7 @@ const ResendVerificationEmail = () => {
 					display: flex;
 					justify-content: center;
 					align-items: center;
-					min-height: calc(100vh - 61px);
+					min-height: calc(100dvh - 61px);
 					background-color: #f7f7f7;
 				}
 
