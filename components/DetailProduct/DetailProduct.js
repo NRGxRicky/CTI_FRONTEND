775c475f25448edad99eb4287d308b28bab9ecf9
aspekwow <aspekwowo@gmail.com>
2024-12-13 10:14:54 -0600

    componente minicart

--- components/DetailProduct/DetailProduct.js
+++ components/DetailProduct/DetailProduct.js
@@ -635,7 +635,7 @@ const DetailProduct = ({
 						width: 100%;
 						display: flex;
 						align-items: center;
-						justify-content: space-evenly;
+						gap: 20px;
 						margin-top: 20px;
 						font-size: 16px;
 					}
