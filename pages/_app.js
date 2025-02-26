b2471dea9dba4e0f1dfe1f6c3b7eb452d4d0f8ee
aspekwow <aspekwowo@gmail.com>
2025-02-25 23:30:25 -0600

    soporte en kueskipay completo

--- pages/_app.js
+++ pages/_app.js
@@ -35,7 +35,7 @@ function MyApp({ Component, pageProps }) {
 				<Head>
 					<meta
 						name='viewport'
-						content='width=device-width, initial-scale=1, maximum-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover'
+						content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover'
 					/>
 				</Head>
 				<AuthProvider>
