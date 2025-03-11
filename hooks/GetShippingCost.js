f0341cee07d26022ffb824afa09088e6ea09c3b3
aspekwow <aspekwowo@gmail.com>
2025-03-11 13:01:59 -0600

    correccion de facebook meta dinamico

--- hooks/GetShippingCost.js
+++ hooks/GetShippingCost.js
@@ -3,32 +3,32 @@ const GetShippingCost = (weightInKg) => {
 
 	if (weight <= 0) {
 		// Podrías decidir si aquí se devuelve 0 o algún valor mínimo
-		return 139;
+		return 129.00;
 	} else if (weight <= 1) {
-		return 139.0;
+		return 129.0;
 	} else if (weight <= 2) {
-		return 145.0;
+		return 133.0;
 	} else if (weight <= 3) {
-		return 151.0;
+		return 138.0;
 	} else if (weight <= 4) {
-		return 159.0;
+		return 143.0;
 	} else if (weight <= 5) {
-		return 165.0;
+		return 149.0;
 	} else if (weight <= 6) {
-		return 170.0;
+		return 154.0;
 	} else if (weight <= 7) {
-		return 177.0;
+		return 160.0;
 	} else if (weight <= 8) {
-		return 182.0;
+		return 165.0;
 	} else if (weight <= 9) {
-		return 188.0;
+		return 171.0;
 	} else if (weight <= 10) {
-		return 194.0;
+		return 177.0;
 	} else {
 		// Para pesos mayores a 10 kg
 		// Redondeo hacia arriba y sumamos 6 MXN por cada kg extra
 		const extraKg = Math.ceil(weight - 10);
-		return 194.0 + extraKg * 6.0;
+		return 177.0 + extraKg * 6.0;
 	}
 };
 
