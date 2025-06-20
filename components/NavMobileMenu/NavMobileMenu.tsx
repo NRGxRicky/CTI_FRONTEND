5d5e938adb3c80fdc1957d7b8afacfd5d586d75a
aspekwow <aspekwowo@gmail.com>
2025-06-20 01:46:46 -0600

    prueba de categorias

--- components/NavMobileMenu/NavMobileMenu.tsx
+++ components/NavMobileMenu/NavMobileMenu.tsx
@@ -336,38 +336,40 @@ const NavMobileMenu = () => {
 								{!loading && !error && (
 									<ul className='mobile-menu__panel-list divide-y divide-gray-200'>
 										{/* Botón de OFERTAS como primer elemento */}
-										<li className='mobile-menu__panel-item'>
-											<Link
-												href={`/listado/all/index?q=&filter_available=true&filter_available_store=false&filter_free_shipping=false&page=1&order=-ventas&filter_discount=true&page_size=${maxPageResults}`}
-												legacyBehavior
-											>
-												<a
-													className='mobile-menu__item-link mobile-menu__item-offers'
-													onClick={toggleMenu}
+										{index === 0 && (
+											<li className='mobile-menu__panel-item'>
+												<Link
+													href={`/listado/all/index?q=&filter_available=true&filter_available_store=false&filter_free_shipping=false&page=1&order=-ventas&filter_discount=true&page_size=${maxPageResults}`}
+													legacyBehavior
 												>
-													<div className='mobile-menu__item-offers-content'>
+													<a
+														className='mobile-menu__item-link mobile-menu__item-offers'
+														onClick={toggleMenu}
+													>
+														<div className='mobile-menu__item-offers-content'>
+															<svg
+																className='mobile-menu__offers-icon'
+																xmlns='http://www.w3.org/2000/svg'
+																viewBox='0 0 24 24'
+																fill='currentColor'
+															>
+																<path d='M13.5.67s.74 2.65.74 4.8c0 2.06-1.35 3.73-3.41 3.73-2.07 0-3.63-1.67-3.63-3.73l.03-.36C5.21 7.51 4 10.62 4 14c0 4.42 3.58 8 8 8s8-3.58 8-8C20 8.61 17.41 3.8 13.5.67zM11.71 19c-1.78 0-3.22-1.4-3.22-3.14 0-1.62 1.05-2.76 2.81-3.12 1.76-.36 3.6-1.21 4.62-2.58.39 1.29.59 2.65.59 4.04 0 2.65-2.15 4.8-4.8 4.8z' />
+															</svg>
+															<span className='mobile-menu__item-text mobile-menu__item-offers-text'>
+																{Capitalize('Ofertas')}
+															</span>
+														</div>
 														<svg
-															className='mobile-menu__offers-icon'
-															xmlns='http://www.w3.org/2000/svg'
+															className='mobile-menu__arrow-icon'
 															viewBox='0 0 24 24'
 															fill='currentColor'
 														>
-															<path d='M13.5.67s.74 2.65.74 4.8c0 2.06-1.35 3.73-3.41 3.73-2.07 0-3.63-1.67-3.63-3.73l.03-.36C5.21 7.51 4 10.62 4 14c0 4.42 3.58 8 8 8s8-3.58 8-8C20 8.61 17.41 3.8 13.5.67zM11.71 19c-1.78 0-3.22-1.4-3.22-3.14 0-1.62 1.05-2.76 2.81-3.12 1.76-.36 3.6-1.21 4.62-2.58.39 1.29.59 2.65.59 4.04 0 2.65-2.15 4.8-4.8 4.8z' />
+															<path d='M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z' />
 														</svg>
-														<span className='mobile-menu__item-text mobile-menu__item-offers-text'>
-															{Capitalize('Ofertas')}
-														</span>
-													</div>
-													<svg
-														className='mobile-menu__arrow-icon'
-														viewBox='0 0 24 24'
-														fill='currentColor'
-													>
-														<path d='M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z' />
-													</svg>
-												</a>
-											</Link>
-										</li>
+													</a>
+												</Link>
+											</li>
+										)}
 
 										{panel.items.map((item) => {
 											const hasSubcategories =
