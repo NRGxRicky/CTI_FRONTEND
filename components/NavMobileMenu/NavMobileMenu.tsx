10896486271990472784d26f680b517c3051f9b8
aspekwow <aspekwowo@gmail.com>
2025-06-21 21:54:39 -0600

    Nuevo Panel de Navegacion lateral

--- components/NavMobileMenu/NavMobileMenu.tsx
+++ components/NavMobileMenu/NavMobileMenu.tsx
@@ -633,7 +633,7 @@ const NavMobileMenu = () => {
 			<style jsx>
 				{`
 					.mobile-menu__container {
-						height: 100vh;
+						height: 100dvh;
 						top: 0;
 						background: #ffffff;
 						position: fixed;
